1d2342f18a1b4f0384d6ee8c40ccef6f02c450f2
Surur Khan <74324419+Rukhan4@users.noreply.github.com>
2021-03-24 14:39:12 -0400

    Delete Data Analysis for Life Sciences directory

--- Data Analysis for Life Sciences/Introduction to Linear Models and Matrix Algebra/Week 3/Expressing_experimental_design_exercise.R
+++
@@ -1,9 +0,0 @@
-day = factor(c("A","B","C"))
-treated = factor(c(2,2,2))
-control = factor(c(2,2,2))
-condition = day
-
-model.matrix(~day + condition)
-model.matrix(~condition)
-model.matrix(~A+B+C+control+treated)
-model.matrix(~day)
